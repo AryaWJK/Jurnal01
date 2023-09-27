340035378af7726efb288445d49451a7cb77fbcb
Arya Wijaksana <aryawijaksana01@gmail.com>
2023-09-27 09:32:56 +0700

    Jurnal 01

---
+++ Jurnal01.java
@@ -0,0 +1,11 @@
+public class Jurnal01 {
+    public static void main(String[] args) {
+        String nama = "Arya Wijaksana";
+        String nim = "607062300047";
+        String kelas = "D3IF-47-04";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Nim: " + nim);
+        System.out.println("Kelas: " + kelas);
+    }
+}
